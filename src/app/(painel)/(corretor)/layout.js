07449ee3b457fd0f593a193ffb1dab49c89234a8
Cyber_Gust <augusto.e.silva@hotmail.com>
2025-11-27 01:03:36 -0300

    Beta 2

--- src/app/(painel)/(corretor)/layout.js
+++ src/app/(painel)/(corretor)/layout.js
@@ -1,7 +1,7 @@
-//src/app/%28corretor%29/layout.js
-
-"use client"; // Necessário para gerenciar o estado da sidebar
-
-import { useState } from "react";
-import clsx from "clsx";
-i
+export default function CorretorLayout({ children }) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
